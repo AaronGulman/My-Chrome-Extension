387cb8fcdc688297e9f63f3736b27791689fc008
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-29 11:03:39 -0500

    Clean the script.js and create the delBtn variable

--- script.js
+++ script.js
@@ -2,34 +2,44 @@ let myLeads = [];
 let btn = document.querySelector('#inputBtn')
 let userInput = document.getElementById('userInput')
 let uol = document.getElementById('ul');
+let delBtn = document.querySelector('#deleteBtn')
+
+delBtn.addEventListener('dblclick', ()=>{
+	localStorage.clear()
+	myLeads = [];
+	renderMyLeads();
+	
+})
+
+function renderMyLeads(){
+	let list = document.createElement('li')
+	uol.appendChild(list);
+	list.style.textDecoration='none';
+let listItems = ''
+for(let i = 0; i < myLeads.length; i++){
+
+listItems +=`
+<li>
+	<a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+ </li>`;
+}
+uol.innerHTML = listItems;
+}
 
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
-console.log(leadsFromLocalStorage)
+
+
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem('myLeads'))
+
+if(leadsFromLocalStorage ){
+	myLeads = leadsFromLocalStorage
+	renderMyLeads()
+}
 
 
 btn.addEventListener('click',()=>{
 myLeads.push(userInput.value)
 userInput.value = "";
-localStorage.getItem("myLeads", JSON.stringify(myLeads));
+localStorage.setItem("myLeads", JSON.stringify(myLeads));
 renderMyLeads();
-// verification
-console.log(localStorage.getItem("myLeads"));
 })
-
-
-function renderMyLeads(){
-let list = document.createElement('li')
-uol.appendChild(list);
-list.style.textDecoration='none';
-let listItems = ''
-for(let i = 0; i < myLeads.length; i++){
-
-	listItems +=`
-	<li>
-		<a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
-	 </li>`;
-	}
-uol.innerHTML = listItems;
-}
-
