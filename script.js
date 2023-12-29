07b9366418d7fb87ed38e3362211adf0b2c11276
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-29 13:24:13 -0500

    Add event listener for tabBtn

--- script.js
+++ script.js
@@ -3,43 +3,60 @@ let btn = document.querySelector('#inputBtn')
 let userInput = document.getElementById('userInput')
 let uol = document.getElementById('ul');
 let delBtn = document.querySelector('#deleteBtn')
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem('myLeads'))
+let tabBtn = document.querySelector('#tab-btn')
+
+
+
+if(leadsFromLocalStorage ){
+	myLeads = leadsFromLocalStorage
+	render(myLeads)
+}
+
+tabBtn.addEventListener('click',function(){
+	chrome.tabs.query({
+		active: true,
+		currentWindow: true,
+		lastFocusedWindow: true
+	    }, function(tabs) {
+		myLeads.push(tabs[0].url)
+		localStorage.setItem("myLeads", JSON.stringify(myLeads));
+		render(myLeads);
+		})
+})
+
+function render(leads){
+	let list = document.createElement('li')
+	uol.appendChild(list);
+	list.style.textDecoration='none';
+	let listItems = ''
+
+
+for(let i = 0; i < leads.length; i++){
+
+listItems +=`
+<li>
+	<a target='_blank' href='${leads[i]}'>${leads[i]}</a>
+ </li>`;
+}
+uol.innerHTML = listItems;
+
+}
+
+// Buttons
 
 delBtn.addEventListener('dblclick', ()=>{
 	localStorage.clear()
 	myLeads = [];
-	renderMyLeads();
+	render(myLeads);
 	
 })
-
-function renderMyLeads(){
-	let list = document.createElement('li')
-	uol.appendChild(list);
-	list.style.textDecoration='none';
-let listItems = ''
-for(let i = 0; i < myLeads.length; i++){
-
-listItems +=`
-<li>
-	<a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
- </li>`;
-}
-uol.innerHTML = listItems;
-}
-
-
-
-
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem('myLeads'))
-
-if(leadsFromLocalStorage ){
-	myLeads = leadsFromLocalStorage
-	renderMyLeads()
-}
-
 
 btn.addEventListener('click',()=>{
 myLeads.push(userInput.value)
 userInput.value = "";
 localStorage.setItem("myLeads", JSON.stringify(myLeads));
-renderMyLeads();
+render(myLeads);
 })
+
+
